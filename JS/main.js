4b8fad0f8d00fcdca894737b224a977f4cca1f68
RJURU <110449011+RJURU@users.noreply.github.com>
2022-09-30 08:19:34 +0100

    Added a modal to show database img instead of having the actual image.

--- JS/main.js
+++ JS/main.js
@@ -202,3 +202,24 @@ if (projectSelect != null) {
         }
     })
 }
+
+
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
