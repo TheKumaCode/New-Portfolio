d02a0933bd71470bf2b5ab2232222e7a17af743d
RJURU <110449011+RJURU@users.noreply.github.com>
2022-09-30 08:46:55 +0100

    Bugfix

--- JS/main.js
+++ JS/main.js
@@ -212,14 +212,3 @@ function openModal() {
 function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
