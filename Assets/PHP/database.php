e81a92fb383d0608d289acba8d7c6bbf28349610
RJURU <robertas.vild9@gmail.com>
2023-01-31 14:31:17 +0000

    Switched database from local to Cpanel.

--- Assets/PHP/database.php
+++ Assets/PHP/database.php
@@ -1,13 +1,13 @@
 <?php 
 
-$dbServerName = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "portfolio";
-
 // $dbServerName = "localhost";
-// $dbUsername = "robertas_vildziunas";
-// $dbPassword = "8H6KgUo0uU~$";
-// $dbName = "robertas_portfolio";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "portfolio";
+
+$dbServerName = "localhost";
+$dbUsername = "robertas_vildziunas";
+$dbPassword = "8H6KgUo0uU~$";
+$dbName = "robertas_portfolio";
 
 $connect = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
